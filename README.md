c495301ba62763542b710038c4bf36458ab11f94
John Ye <john.ye@intel.com>
2014-01-27 17:22:05 +0800

    a simpler example

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ push relabel algorithm for maxflow/mincut graph cut
 Reference:
 
 http://community.topcoder.com/tc?module=Static&d1=tutorials&d2=maxflowPushRelabel
+
+http://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=10&sqi=2&ved=0CHcQFjAJ&url=http%3A%2F%2Fmelodi.ee.washington.edu%2F~bilmes%2Fgrg%2Fpushrelabel1.ppt&ei=O-DlUoPOD4zroATYvYG4CA&usg=AFQjCNGX4x-gN6YjkXjGM2aFGVC5VYIN4g&sig2=Na51SMJ2MEgj2ipyuZmhqA&cad=rja
--- gc.lua
+++ gc.lua
@@ -1,4 +1,38 @@
-dofile("test.txt")
+#!/usr/bin/env lua5.1
+
+dofile(arg[1])
+
+function show_cap(g)
+   for u, l in pairs(g) do
+      print(u)
+      for v, c in pairs(l) do
+	 print("", v, c)
+      end
+   end
+end
+
+function show_height(h)
+   for i, v in pairs(h) do
+      print(i, v)
+   end
+end
+
+function show_flow(f)
+   for u, l in pairs(f) do
+      print(u)
+      for v, x in pairs(l) do
+	 print('', v, x)
+      end
+   end
+end
+
+function show_excess(e)
+   for i, v in pairs(e) do
+      print(i, v)
+   end
+end
+
+show_cap(G)
 
 function mset(m, i, j, v)
    if not m[i] then m[i]={} end
@@ -18,17 +52,27 @@ cf={}
 function init_preflow(c)
    h['S'] = #c
 
-   for v, x in c.S do      
+   for v, x in pairs(c.S) do      
       mset(f, 'S', v, x)
-      mset(f, v, 'S', -v)
+      mset(f, v, 'S', -x)
       e[v] = x
-      e['S'] = (e['S'] or 0) - v
+      e['S'] = (e['S'] or 0) - x
       mset(cf, 'S', v, x - f['S'][v])
-      mset(cf, v, 'S', -v - f[v]['S'])
+      mset(cf, v, 'S', -x - f[v]['S'])
    end
 end
 
-function push(u v)
+init_preflow(G)
+
+print("Height:")
+show_height(h)
+print("Preflow:")
+show_flow(f)
+print("Excess:")
+show_excess(e)
+
+
+function push(u, v)
    local temp = (e[u] < cf[u][v]) and e[u] or cf[u][v]
    mset(f, u, v, f[u][v] + temp)
    mset(f, v, u, -f[u][v])
