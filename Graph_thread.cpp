d6a89878bee1c649beccbccceb155f8fedd09828
Toe Zju <zjutoe@gmail.com>
2014-02-10 17:42:46 +0800

    multithread WIP

--- Graph_thread.cpp
+++ Graph_thread.cpp
@@ -1,11 +1,15 @@
 // copy from http://jqyblogger.blogspot.com/2012/06/sample-c-code-for-push-relabel-max-flow.html
 
 #include <climits>
+#include<pthread.h>
+#include <deque>
 #include "Graph.h"
 
 
 Graph::Graph()// s and t represent the index of source node and sink node
 {
+	clk = 0;		// global clock
+
 	int i,j;
 	// source = s;
 	// sink = t;
@@ -32,37 +36,81 @@ Graph::Graph()// s and t represent the index of source node and sink node
 	cout<<"please input the matrix of capacity!"<<endl;
 	cMatrixR = new int[H][W];
 	cMatrixD = new int[H][W];
+	cMatrixL = new int[H][W];
+	cMatrixU = new int[H][W];
+
 	fMatrixR = new int[H][W];
 	fMatrixD = new int[H][W];
+	fMatrixL = new int[H][W];
+	fMatrixU = new int[H][W];
+
 	rMatrixR = new int[H][W];
 	rMatrixD = new int[H][W];
+	rMatrixL = new int[H][W];
+	rMatrixU = new int[H][W];
+
 	hMatrix  = new int[H][W];
 	eMatrix  = new int[H][W];
 
 	// the input should interleave the capacity R and D line by
 	// line
 	for (i=0; i<H; i++) {
+		int c;
+
 		for (j=0; j<W; j++) {
-			cin>>cMatrixR[i][j];
-			rMatrixR[i][j] = cMatrixR[i][j];
+			cin>>c;
+			cMatrixR[i][j] = c;
+			cMatrixL[i][j] = -c;
+			
+			//rMatrixR[i][j] = cMatrixR[i][j];
 		}
 		for (j=0; j<W; j++) {
-			cin>>cMatrixD[i][j];
-			rMatrixD[i][j] = cMatrixD[i][j];
+			cin>>c;
+			cMatrixD[i][j] = c;
+			cMatrixU[i][j] = -c;
+			
+			//rMatrixD[i][j] = cMatrixD[i][j];
 		}
 	}
+	
 
-	memset(fMatrixR, 0, sizeof(int)*nodenumber);
-	memset(fMatrixD, 0, sizeof(int)*nodenumber);
+	memset(fMatrixR, 0, sizeof(int)*nodeNumber);
+	memset(fMatrixD, 0, sizeof(int)*nodeNumber);
+	memset(fMatrixL, 0, sizeof(int)*nodeNumber);
+	memset(fMatrixU, 0, sizeof(int)*nodeNumber);
 
 	fMatrixR[si][sj]  = cMatrixR[si][sj];
 	fMatrixD[si][sj]  = cMatrixD[si][sj];
+	fMatrixL[si][sj]  = -fMatrixR[si][sj];
+	fMatrixU[si][sj]  = -fMatrixD[si][sj];
+
 	eMatrix[si][sj+1] = fMatrixR[si][sj]; // (si, sj) -> (si, sj+1), i.e. flow to right
 	eMatrix[si+1][sj] = fMatrixD[si][sj]; // (si, sj) -> (si+1, sj), i.e. flow to down	
 	eMatrix[si][sj]   = -(fMatrixR[si][sj] + fMatrixD[si][sj]);
-	rMatrixR[si][sj]  = 0;
-	rMatrixD[si][sj]  = 0;
-	hMatrix[si][sj]   = nodenumber;	
+
+	hMatrix[si][sj]   = nodeNumber;
+
+	// build residual 
+	int f;
+	for (i=0; i<H; i++) {
+		for (j=0; j<W; j++) {
+			f = fMatrixR[i][j];
+			c = cMatrixR[i][j];
+			rMatrixR[i][j] = f>=0 ? c-f : -f;
+
+			f = fMatrixL[i][j];
+			c = cMatrixL[i][j];
+			rMatrixR[i][j] = f>=0 ? c-f : -f;
+
+			f = fMatrixD[i][j];
+			c = cMatrixD[i][j];
+			rMatrixR[i][j] = f>=0 ? c-f : -f;
+
+			f = fMatrixU[i][j];
+			c = cMatrixU[i][j];
+			rMatrixR[i][j] = f>=0 ? c-f : -f;
+		}
+	}
 
 	// for(i = 0;i<nodeNumber;i++) {
 	// 	if(cMatrix[s][i]!=0) {
@@ -96,6 +144,8 @@ void* worker(void* para)
 
 	int cnt = 0;
 	
+	cout<<"worker ("<<i<<","<<j<<") start"<<endl;
+
 	while (clk >= 0) {
 		if (cnt < clk) {
 			// do the push relabel work
@@ -103,50 +153,68 @@ void* worker(void* para)
 			// 2. check whether the current node can push some flow out
 			// 3. if can push, then push
 			// 4. if cannot push, then pull, and relabel
+			
+			if (clk % 2) { // we only pull in odd clock cycles
+				int pv = 0;
+				if (j>0 && g->pushMatrixR[i][j-1]>0) {
+					pv += g->pushMatrixR[i][j-1];
+					g->pushMatrixR[i][j-1] = 0;
+				}
+				if (j<W-1 && g->pushMatrixL[i][j+1]>0) {
+					pv += g->pushMatrixL[i][j+1];
+					g->pushMatrixL[i][j+1] = 0;
+				}
+				if (i>0 && g->pushMatrixD[i-1][j]>0) {
+					pv += g->pushMatrixD[i-1][j];
+					g->pushMatrixD[i-1][j] = 0;
+				}
+				if (i<H-1 && g->pushMatrixU[i+1][j]>0) {
+					pv += g->pushMatrixU[i+1][j];
+					g->pushMatrixU[i+1][j] = 0;
+				}
 
-			int excess = g->eMatrix[i][j];
-			if (excess > 0) { // active
-				int** pushMatrixR = g->pushMatrixR;
-				int** pushMatrixD = g->pushMatrixD;
-				int** hMatrix = g->hMatrix;
-				int** eMatrix = g->eMatrix;
-				int** rMatrixR = g->rMatrixR;
-				int** rMatrixD = g->rMatrixD;
+				eMatrix[i][j] += pv;
 				
-				if (g->rMatrixR[i][j] > 0 && hMatrix[i][j] == hMatrix[i][j+1]+1)
-					g->pushMatrixR[i][j] += min(excess, g->rMatrixR[i][j]); // push Right
-				else if (g->rMatrixD[i][j] > 0 && hMatrix[i][j] == hMatrix[i+1][j]+1)
-					g->pushMatrixD[i][j] += min(excess, g->rMatrixD[i][j]); // push Down
-				else if (g->rMatrixR[i][j-1] < 0 && hMatrix[i][j] == hMatrix[i][j-1])
-					g->pushMatrixR[i][j-1] -= min(excess, g->rMatrixR[i][j-1]); // push Left
-				else if (g->rMatrixD[i-1][j] < 0 && hMatrix[i][j] == hMatrix[i-1][j]+1)
-					g->pushMatrixD[i-1][j] -= min(excess, g->rMatrixD[i-1][j]); // push Up
-				else { // need relabel
-					// pull 
-					int pv = 0;
-					pv += pushMatrixR[i][j-1]; pushMatrixR[i][j-1] = 0;
-					pv -= pushMatrixR[i][j]; pushMatrixR[i][j] = 0;
-					pv += pushMatrixD[i-1][j]; pushMatrixD[i-1][j] = 0;
-					pv -= pushMatrixD[i][j]; pushMatrixD[i][j] = 0;
+			} else {
+				int excess = g->eMatrix[i][j];
+				if (excess > 0) { // active
+					int** pushMatrixR = g->pushMatrixR;
+					int** pushMatrixL = g->pushMatrixL;
+					int** pushMatrixD = g->pushMatrixD;
+					int** pushMatrixU = g->pushMatrixU;
+					int** hMatrix = g->hMatrix;
+					int** eMatrix = g->eMatrix;
+					int** rMatrixR = g->rMatrixR;
+					int** rMatrixD = g->rMatrixD;
+					int** rMatrixL = g->rMatrixL;
+					int** rMatrixU = g->rMatrixU;
+					
+					int r, d, l, u;
+					r = g->push_R(i, j);
+					d = g->push_D(i, j);
+					l = g->push_L(i, j);
+					u = g->push_U(i, j);
+					if (r==-1 && d==-1 && l==-1 && u==-1) {
+						// need relabel
+						
+						int temp = INT_MAX;
+						if (rMatrixR[i][j] > 0 && hMatrix[i][j+1] < temp)
+							temp = hMatrix[i][j+1];
+						else if (rMatrixR[i][j-1] < 0 && hMatrix[i][j-1] < temp)
+							temp = hMatrix[i][j-1];
+						else if (rMatrixD[i][j] > 0 && hMatrix[i+1][j] < temp)
+							temp = hMatrix[i+1][j];
+						else if (rMatrixD[i-1][j] < 0 && hMatrix[i-1][j] < temp)
+							temp = hMatrix[i-1][j];
 
-					// relabel
-					int temp = INT_MAX;
-					if (rMatrixR[i][j] > 0 && hMatrix[i][j+1] < temp)
-						temp = hMatrix[i][j+1];
-					else if (rMatrixR[i][j-1] < 0 && hMatrix[i][j-1] < temp)
-						temp = hMatrix[i][j-1];
-					else if (rMatrixD[i][j] > 0 && hMatrix[i+1][j] < temp)
-						temp = hMatrix[i+1][j];
-					else if (rMatrixD[i-1][j] < 0 && hMatrix[i-1][j] < temp)
-						temp = hMatrix[i-1][j];
-
-					hMatrix[i][j] = temp + 1; // toe paused here
+						hMatrix[i][j] = temp + 1;
+					}
 				}
 			}
 
-			cnt++;
+			cnt++;	// catch up wit the current clock
 		}		
-		sleep(100);
+		std::this_thread::sleep_for (std::chrono::seconds(1));
 	}
 }
 
@@ -163,6 +231,121 @@ int Graph::checkNode(int i)//check whether the excess node i can push some flow 
 	return -1;
 }
 
+int Graph::do_push_R(int i, int j, int delta)
+{
+	pushMatrixR[i][j] += delta;
+	pushMatrixL[i][j+1] -= delta;
+
+	rMatrixR[i][j]   -= delta;
+	eMatrix[i][j]    -= delta;
+	rMatrixL[i][j+1] += delta;
+	// eMatrix[i][j+1] += delta;  do in pull stage
+
+	if (cMatrixR[i][j] > 0) {
+		fMatrixR[i][j] += delta;
+		fMatrixL[i][j+1] = -fMatrixR[i][j];
+	} else {
+		fMatrixL[i][j+1] -= delta;
+		fMatrixR[i][j] = -fMatrixL[i][j+1];
+	}
+
+	return 0;
+}
+
+int Graph::do_push_L(int i, int j, int delta)
+{
+	return do_push_R(i, j-1, -delta);
+}
+
+int Graph::do_push_D(int i, int j, int delta)
+{
+	pushMatrixD[i][j] += delta;
+	pushMatrixU[i+1][j] -= delta;
+
+	rMatrixD[i][j]   -= delta;
+	eMatrix[i][j]    -= delta;
+	rMatrixU[i+1][j] += delta;
+	// eMatrix[i+1][j] += delta;  do in pull stage
+
+	if (cMatrixD[i][j] > 0) {
+		fMatrixD[i][j] += delta;
+		fMatrixU[i+1][j] = -fMatrixD[i][j];
+	} else {
+		fMatrixU[i+1][j] -= delta;
+		fMatrixD[i][j] = -fMatrixU[i][j+1];
+	}
+
+	return 0;
+}
+
+int Graph::do_push_U(int i, int j, int delta)
+{
+	return do_push_D(i-1, j, -delta);
+}
+
+int Graph::push_R(int i, int j)
+{
+	if (j >= W-1) return -1;
+	if (hMatrix[i][j] != hMatrix[i][j+1] + 1) return -1;
+
+	int excess   = eMatrix[i][j];
+	int residual = rMatrixR[i][j];
+	int delta    = (excess <= residual) ? excess : residual;
+
+	return do_push_R(i, j, delta);
+
+	// pushMatrixR[i][j] += delta;
+	// pushMatrixL[i][j+1] -= delta;
+
+	// rMatrixR[i][j]   -= delta;
+	// eMatrix[i][j]    -= delta;
+	// rMatrixL[i][j+1] += delta;
+	// // eMatrix[i][j+1] += delta;  do in pull stage
+
+	// if (cMatrixR[i][j] > 0) {
+	// 	fMatrixR[i][j] += delta;
+	// 	fMatrixL[i][j+1] = -fMatrixR[i][j];
+	// } else {
+	// 	fMatrixL[i][j+1] -= delta;
+	// 	fMatrixR[i][j] = -fMatrixL[i][j+1];
+	// }
+}
+
+int Graph::push_L(int i, int j)
+{
+	if (j <= 0) return -1;
+	if (hMatrix[i][j] != hMatrix[i][j-1] + 1) return -1;
+
+	int excess   = eMatrix[i][j];
+	int residual = rMatrixR[i][j];
+	int delta    = (excess <= residual) ? excess : residual;
+
+	return do_push_L(i, j, delta);
+}
+
+int Graph::push_D(int i, int j)
+{
+	if (i >= H-1) return -1;
+	if (hMatrix[i][j] != hMatrix[i+1][j] + 1) return -1;
+
+	int excess   = eMatrix[i][j];
+	int residual = rMatrixR[i][j];
+	int delta    = (excess <= residual) ? excess : residual;
+
+	return do_push_D(i, j, delta);
+}
+
+int Graph::push_U(int i, int j)
+{
+	if (i <= 0) return -1;
+	if (hMatrix[i][j] != hMatrix[i-1][j] + 1) return -1;
+
+	int excess   = eMatrix[i][j];
+	int residual = rMatrixR[i][j];
+	int delta    = (excess <= residual) ? excess : residual;
+
+	return do_push_U(i, j, delta);
+}
 
 void Graph::push(int a, int b)//push excess flow from node i to node j
 {
@@ -228,26 +411,45 @@ void Graph::relable(int a)//relable this node a
 
 void Graph::mineMaxFlow()
 {
-	int current,i,j,target;
-	while(excessNode.size()!=0) {
-		current = excessNode.at(0);
-		excessNode.pop_front();
-		target = checkNode(current);
-		if(checkNode(current)<0) {
-			relable(current);
-		} else {
-			push(current,target);
+	// int current,i,j,target;
+	// while(excessNode.size()!=0) {
+	// 	current = excessNode.at(0);
+	// 	excessNode.pop_front();
+	// 	target = checkNode(current);
+	// 	if(checkNode(current)<0) {
+	// 		relable(current);
+	// 	} else {
+	// 		push(current,target);
+	// 	}
+	// }
+
+	deque<pthread_t*> threads;
+
+	int i, j;
+	for (i=0; i<H; i++) {
+		for (j=0; j<W; j++) {
+			pthread_t thd;
+			arg_t arg;
+			arg.g = this;
+			arg.i = i;
+			arg.j = j;
+			pthread_create(&thd, NULL, worker, NULL, &arg);
+			threads.push_back(&thd);
 		}
 	}
 
-
-	cout<<"the final flow is like:"<<endl;
-	for(i = 0;i<nodeNumber;i++) {
-		for(j = 0;j<nodeNumber;j++) {
-			cout<<fMatrix[i][j]<<" ";
-		}
-		cout<<endl;
+	for (i=0; i<5; i++) {
+		clk++;
+		std::this_thread::sleep_for(std::chrono::seconds(3));
 	}
+
+	// cout<<"the final flow is like:"<<endl;
+	// for(i = 0;i<nodeNumber;i++) {
+	// 	for(j = 0;j<nodeNumber;j++) {
+	// 		cout<<fMatrix[i][j]<<" ";
+	// 	}
+	// 	cout<<endl;
+	// }
 }
 
 void Graph::residualBuilder()
--- Graph_thread.h
+++ Graph_thread.h
@@ -13,17 +13,28 @@ using std::endl;
 class Graph
 {
 private:
-int ** cMatrix;//capacity matrix
-int ** fMatrix;//flow
-int ** rMatrix;//residual flow
+int ** cMatrixR;//capacity matrix
+int ** cMatrixL;//capacity matrix
+int ** cMatrixD;//capacity matrix
+int ** cMatrixU;//capacity matrix 
+int ** fMatrixR;//flow
+int ** fMatrixL;//flow
+int ** fMatrixD;//flow
+int ** fMatrixU;//flow
+int ** rMatrixR;//residual flow
+int ** rMatrixL;//residual flow
+int ** rMatrixD;//residual flow
+int ** rMatrixU;//residual flow
 int ** hMatrix;//height function
-int * excess;//the excess of each node
+int ** eMatrix;//the excess of each node
 deque<int> excessNode;
-int X;
-int Y;
+int W;
+int H;
 int nodeNumber;
 int source;
 int sink;
+
+int clk;
 
 
 public:
@@ -35,5 +46,9 @@ int checkNode(int);
 void push(int, int);
 void relable(int);
 void mineMaxFlow();
+int Graph::push_R(int i, int j);
+int Graph::push_L(int i, int j);
+int Graph::push_D(int i, int j);
+int Graph::push_U(int i, int j);
 
 };
